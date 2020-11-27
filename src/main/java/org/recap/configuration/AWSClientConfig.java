bf274529fd833c7a959d47fbd82f07f46a21e789
rmaheswaran <rmaheswaran@ciber.com>
2020-11-27 16:32:42 +0530

    Removing aws s3 hardcoded details credentils and gettting from db

--- src/main/java/org/recap/configuration/AWSClientConfig.java
+++ src/main/java/org/recap/configuration/AWSClientConfig.java
@@ -6,17 +6,23 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AWSClientConfig {
 
+    @Value("${awsAccessKey}")
+    private String awsAccessKey;
+
+    @Value("${awsAccessSecretKey}")
+    private String awsAccessSecretKey;
+
     @Bean
     public AmazonS3 getAwsClient() {
         AWSCredentials credentials = new BasicAWSCredentials(
-                "AKIA45ITQHSNWAWZU6AQ",
-                "hg90O8iyPVGbHr1a7qc5+KxqtjRlWHAMZFzyqHUx"
+                awsAccessKey, awsAccessSecretKey
         );
 
         AmazonS3 awsS3Client = AmazonS3ClientBuilder
