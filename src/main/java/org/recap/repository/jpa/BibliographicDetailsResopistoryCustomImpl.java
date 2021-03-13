35bfa337395b3dfc2afcc86027f59bf7a9cad93c
HariKrishnan-19 <harikrishnan.v@htcindia.com>
2021-03-13 15:14:13 +0530

    SCSB-540 : Refractored to remove mutuability inside streams

--- src/main/java/org/recap/repository/jpa/BibliographicDetailsResopistoryCustomImpl.java
+++ src/main/java/org/recap/repository/jpa/BibliographicDetailsResopistoryCustomImpl.java
@@ -1,11 +1,13 @@
 package org.recap.repository.jpa;
 
 import org.recap.model.jpa.BibliographicEntity;
+import org.recap.model.jpa.HoldingsEntity;
 import org.recap.model.jpa.ItemEntity;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Optional;
 
 @Transactional
 public class BibliographicDetailsResopistoryCustomImpl implements BibliographicDetailsResopistoryCustom {
@@ -15,29 +17,32 @@ public class BibliographicDetailsResopistoryCustomImpl implements BibliographicD
 
     @Override
     public <S extends BibliographicEntity> S updateBibForSubmitCollection(S entity, ItemEntity fetchedItemEntity) {
-            setHoldingsAndItemId(entity, fetchedItemEntity);
-            return entityManager.merge(entity);
+        attachExistingHoldingAndItemId(entity, fetchedItemEntity);
+        return entityManager.merge(entity);
     }
 
-    private <S extends BibliographicEntity> void setHoldingsAndItemId(S entity, ItemEntity fetchedItemEntity) {
-        Integer holdingsId = fetchedItemEntity.getHoldingsEntities().get(0).getId();
-        Integer itemId = fetchedItemEntity.getId();
-
-        setHoldingsId(entity, holdingsId);
-        setItemId(entity, itemId);
+    private <S extends BibliographicEntity> void attachExistingHoldingAndItemId(S entity, ItemEntity fetchedItemEntity) {
+        attachExistingHoldingId(entity, fetchedItemEntity);
+        attachExistingItemId(entity, fetchedItemEntity);
     }
 
-    private <S extends BibliographicEntity> void setItemId(S entity, Integer itemId) {
-        entity.getItemEntities()
+    private <S extends BibliographicEntity> void attachExistingItemId(S entity, ItemEntity fetchedItemEntity) {
+        Optional<ItemEntity> itemEntityToAttach = entity.getItemEntities()
                 .stream()
-                .filter(itemEntity -> itemEntity.getId()==null)
-                .forEach(itemEntity -> itemEntity.setId(itemId));
+                .filter(itemEntity -> itemEntity.getId() == null)
+                .findFirst();
+        if(itemEntityToAttach.isPresent()){
+            itemEntityToAttach.get().setId(fetchedItemEntity.getId());
+        }
     }
 
-    private <S extends BibliographicEntity> void setHoldingsId(S entity, Integer holdingsId) {
-        entity.getHoldingsEntities()
+    private <S extends BibliographicEntity> void attachExistingHoldingId(S entity, ItemEntity fetchedItemEntity) {
+        Optional<HoldingsEntity> holdingsEntityToAttach = entity.getHoldingsEntities()
                 .stream()
                 .filter(holdingsEntity -> holdingsEntity.getId() == null)
-                .forEach(holdingsEntity -> holdingsEntity.setId(holdingsId));
+                .findFirst();
+        if(holdingsEntityToAttach.isPresent()){
+            holdingsEntityToAttach.get().setId(fetchedItemEntity.getHoldingsEntities().get(0).getId());
+        }
     }
 }
