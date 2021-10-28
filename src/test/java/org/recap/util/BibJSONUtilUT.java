34be9b7506b8862d93aa5f1533bf6b1fc0776976
JancyRoachR <jancy.roach@htcindia.com>
2021-10-28 11:49:35 +0530

    UT changes Stage4##6

--- src/test/java/org/recap/util/BibJSONUtilUT.java
+++ src/test/java/org/recap/util/BibJSONUtilUT.java
@@ -1,9 +1,13 @@
 package org.recap.util;
 
+import info.freelibrary.marc4j.impl.ControlFieldImpl;
 import org.apache.camel.ProducerTemplate;
 import org.junit.Before;
 import org.junit.Test;
+import org.marc4j.marc.DataField;
 import org.marc4j.marc.Record;
+import org.marc4j.marc.Subfield;
+import org.marc4j.marc.VariableField;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -27,13 +31,22 @@ public class BibJSONUtilUT extends BaseTestCaseUT {
     MarcUtil marcUtil;
 
     @Mock
-    Record record ;
+    Record record;
+
+    @Mock
+    DataField dataField;
+
+    @Mock
+    ControlFieldImpl controlField;
+
+    @Mock
+    Subfield subfield;
 
     @Mock
     ProducerTemplate producerTemplate;
 
     @Before
-    public void setup(){
+    public void setup() {
         bibJSONUtil.setNonHoldingInstitutions(Arrays.asList("PUL"));
         bibJSONUtil.setProducerTemplate(producerTemplate);
         bibJSONUtil.getNonHoldingInstitutions();
@@ -41,45 +54,77 @@ public class BibJSONUtilUT extends BaseTestCaseUT {
     }
 
     @Test
-    public void getLCCNValue(){
+    public void getLCCNValue() {
         Mockito.when(marcUtil.getDataFieldValue(any(), any(), any(), any(), any())).thenReturn("test");
         String result = bibJSONUtil.getLCCNValue(record);
         assertNotNull(result);
     }
 
     @Test
-    public void getISBNNumber(){
+    public void getISBNNumber() {
         List<String> isbnNumberList = new ArrayList<>();
         isbnNumberList.add("233");
         Mockito.when(marcUtil.getMultiDataFieldValues(any(), anyString(), any(), any(), anyString())).thenReturn(isbnNumberList);
+        List<VariableField> dataFields = new ArrayList<>();
+        dataFields.add(dataField);
+        Mockito.when(record.getVariableFields(anyString())).thenReturn(dataFields);
+        Mockito.when(dataField.getSubfields(anyString())).thenReturn(Arrays.asList(subfield));
+        Mockito.when(subfield.getData()).thenReturn("isbnNumbers");
         List<String> result = bibJSONUtil.getISBNNumber(record);
         assertNotNull(result);
     }
 
     @Test
-    public void getISSNNumber(){
+    public void getISSNNumber() {
         List<String> isbnNumberList = new ArrayList<>();
         isbnNumberList.add("233");
         Mockito.when(marcUtil.getMultiDataFieldValues(any(), anyString(), any(), any(), anyString())).thenReturn(isbnNumberList);
+        List<VariableField> dataFields = new ArrayList<>();
+        dataFields.add(dataField);
+        Mockito.when(record.getVariableFields(anyString())).thenReturn(dataFields);
+        Mockito.when(dataField.getSubfields(anyString())).thenReturn(Arrays.asList(subfield));
+        Mockito.when(subfield.getData()).thenReturn("issnNumbers");
         List<String> result = bibJSONUtil.getISSNNumber(record);
         assertNotNull(result);
     }
 
     @Test
-    public void getTitle(){
+    public void getTitle() {
         String result = bibJSONUtil.getTitle(record);
         assertNotNull(result);
     }
 
     @Test
-    public void getOCLCNumbers(){
+    public void getOCLCNumbers() {
         String institutionCode = "PUL";
         List<String> oclcNumberList = new ArrayList<>();
         oclcNumberList.add("OCoLC");
         List<String> nonHoldingInstitutions = new ArrayList<>();
         nonHoldingInstitutions.add("PUL");
-        ReflectionTestUtils.setField(bibJSONUtil,"nonHoldingInstitutions",nonHoldingInstitutions);
-        List<String> result = bibJSONUtil.getOCLCNumbers(record,institutionCode);
+        List<VariableField> dataFields = new ArrayList<>();
+        dataFields.add(dataField);
+        Mockito.when(record.getVariableFields(anyString())).thenReturn(dataFields);
+        Mockito.when(dataField.getSubfields(anyString())).thenReturn(Arrays.asList(subfield));
+        Mockito.when(subfield.getData()).thenReturn("OCoLC");
+        ReflectionTestUtils.setField(bibJSONUtil, "nonHoldingInstitutions", nonHoldingInstitutions);
+        List<String> result = bibJSONUtil.getOCLCNumbers(record, institutionCode);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void getOCLCNumbersForControlFieldValue() {
+        String institutionCode = "PUL";
+        List<String> oclcNumberList = new ArrayList<>();
+        oclcNumberList.add("OCoLC");
+        List<String> nonHoldingInstitutions = new ArrayList<>();
+        nonHoldingInstitutions.add("PUL");
+        List<VariableField> dataFields = new ArrayList<>();
+        dataFields.add(controlField);
+        Mockito.when(record.getVariableFields("003")).thenReturn(dataFields);
+        Mockito.when(record.getVariableFields("001")).thenReturn(dataFields);
+        Mockito.when(controlField.getData()).thenReturn("OCoLC");
+        ReflectionTestUtils.setField(bibJSONUtil, "nonHoldingInstitutions", nonHoldingInstitutions);
+        List<String> result = bibJSONUtil.getOCLCNumbers(record, institutionCode);
         assertNotNull(result);
     }
 }
